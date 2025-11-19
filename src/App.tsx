9060af2d481e148334645de76e83172500c23786
Ign Ven <ignas.venckunas@gmail.com>
2025-11-19 19:15:44 +0200

    analytics vercel react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import Mid from "./components/Mid/Mid";
 import Skils from "./components/Skils/Skils";
 import Top from "./components/Top/Top";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
